9184a69cb6b925c52b6f3a837639784d2e94f3aa
readwall <readwall@163.com>
2013-07-30 21:10:16 +0800

    asdf

---
+++ test/src/com/test/test/Test.java
@@ -0,0 +1,61 @@
+/**
+ * <p>文件名:		Test.java</p>
+ * <p>版权:		CopyrightTag</p>
+ * <p>公司:		CompanyTag</p>
+ * @author		李德强(lideqiang@nvlbs.com, readwall@163.com)
+ */
+
+package com.test.test;
+
+/**
+ * <p>
+ * Test
+ * </p>
+ * <p>
+ * TODO
+ * </p>
+ * 
+ * @author 李德强(lideqiang@nvlbs.com, readwall@163.com)
+ * @version 0.0.0
+ *          <table style="border:1px solid gray;">
+ *          <tr>
+ *          <th width="100px">版本号</th>
+ *          <th width="100px">动作</th>
+ *          <th width="100px">修改人</th>
+ *          <th width="100px">修改时间</th>
+ *          </tr>
+ *          <!-- 以 Table 方式书写修改历史 -->
+ *          <tr>
+ *          <td>0.0.0</td>
+ *          <td>创建类</td>
+ *          <td>李德强</td>
+ *          <td>2013-7-30 下午9:09:55</td>
+ *          </tr>
+ *          <tr>
+ *          <td>XXX</td>
+ *          <td>XXX</td>
+ *          <td>XXX</td>
+ *          <td>XXX</td>
+ *          </tr>
+ *          </table>
+ */
+public class Test {
+
+	/**
+	 * 构造器
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * FIXME
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
